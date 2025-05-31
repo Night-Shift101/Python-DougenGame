5f2ced36f3ac6df692f8c77be0ffccf457309f71
Night-Shift101 <24gavinfox@gmail.com>
2025-05-31 16:32:19 -0400

    QOL Features

--- game.py
+++ game.py
@@ -6,6 +6,5 @@ from gameWindow import GameWindow
 defaultSettings = [31, 1, .2]
 
 map = Map(defaultSettings[0],defaultSettings[1],defaultSettings[2])
-map.printMap()
 main = GameWindow(map)
 main.window.mainloop()
--- gameWindow.py
+++ gameWindow.py
@@ -4,17 +4,20 @@ class GameWindow:
     def __init__(self, map):
         self.window = tk.Tk()
         self.window.title("My Tkinter Window")
-        self.window.geometry("400x300")
+        self.window.geometry("700x600")
         self.map = map
         self.label = tk.Label(self.window, text=self.map.printMap(), font='TkFixedFont')
         self.label.pack()
 
-        self.regenerate = tk.Button(self.window, command=self._regenerate )
+        self.regenerate = tk.Button(self.window, command=self._regenerate, text="New Map", font='TkFixedFont' )
         self.regenerate.pack()
     def _regenerate(self):
         self.map.regenerate()
         self.label.destroy()
+        self.regenerate.destroy()
         self.label = tk.Label(self.window, text=self.map.printMap(), font='TkFixedFont')
         self.label.pack()
+        self.regenerate = tk.Button(self.window, command=self._regenerate, text="New Map", font='TkFixedFont' )
+        self.regenerate.pack()
 
         
